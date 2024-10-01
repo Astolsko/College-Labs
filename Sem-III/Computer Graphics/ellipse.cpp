76bbc81a19e345f70bb998b47460ab475bd3d0ec
Astolsko <82147523+Astolsko@users.noreply.github.com>
2024-10-01 10:34:21 +0530

    update computer graphics lab

---
+++ Sem-III/Computer Graphics/ellipse.cpp
@@ -0,0 +1,88 @@
+// Ellipse using mid point algorithm
+#include <graphics.h>
+#include <stdio.h>
+#include <math.h>
+
+void drawEllipse(int xc, int yc, int rx, int ry)
+{
+    float dx, dy, d1, d2, x, y;
+    x = 0;
+    y = ry;
+
+    // Decision parameter for region 1
+    d1 = (ry * ry) - (rx * rx * ry) + (0.25 * rx * rx);
+    dx = 2 * ry * ry * x;
+    dy = 2 * rx * rx * y;
+
+    // For region 1
+    while (dx < dy)
+    {
+        // Draw the points in all four quadrants
+        putpixel(xc + x, yc + y, WHITE);
+        putpixel(xc - x, yc + y, WHITE);
+        putpixel(xc + x, yc - y, WHITE);
+        putpixel(xc - x, yc - y, WHITE);
+
+        // Checking and updating decision parameter
+        if (d1 < 0)
+        {
+            x++;
+            dx = dx + (2 * ry * ry);
+            d1 = d1 + dx + (ry * ry);
+        }
+        else
+        {
+            x++;
+            y--;
+            dx = dx + (2 * ry * ry);
+            dy = dy - (2 * rx * rx);
+            d1 = d1 + dx - dy + (ry * ry);
+        }
+    }
+
+    // Decision parameter for region 2
+    d2 = ((ry * ry) * ((x + 0.5) * (x + 0.5))) + ((rx * rx) * ((y - 1) * (y - 1))) - (rx * rx * ry * ry);
+
+    // Plotting points of region 2
+    while (y >= 0)
+    {
+        // Draw the points in all four quadrants
+        putpixel(xc + x, yc + y, WHITE);
+        putpixel(xc - x, yc + y, WHITE);
+        putpixel(xc + x, yc - y, WHITE);
+        putpixel(xc - x, yc - y, WHITE);
+
+        // Checking and updating decision parameter
+        if (d2 > 0)
+        {
+            y--;
+            dy = dy - (2 * rx * rx);
+            d2 = d2 + (rx * rx) - dy;
+        }
+        else
+        {
+            y--;
+            x++;
+            dx = dx + (2 * ry * ry);
+            dy = dy - (2 * rx * rx);
+            d2 = d2 + dx - dy + (rx * rx);
+        }
+    }
+}
+
+int main()
+{
+    // Initialize graphics mode
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "C:\\Turboc3\\BGI");
+
+    int xc = 200, yc = 200;  // center of ellipse
+    int rx = 100, ry = 50;   // radii along x and y
+
+    drawEllipse(xc, yc, rx, ry);
+
+    getch();  // Wait for user input
+    closegraph();  // Close the graphics window
+
+    return 0;
+}
